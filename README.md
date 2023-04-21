887cda9367aa03baae3d3d2158679089cb2c4331
Harry <hoobs404@gmail.com>
2023-04-22 00:06:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Requirements:
+Nodejs v16+
+discord-rpc
+.env
+
+Exmple env file:
+
+```
+#.env
+applicationID = "ABCDEFG12345678"
+label = "LabelOne"
+url = "Url"
+labelTwo = "labelTwo"
+urlTwo = "UrlTwo"
+details = "Activity Details"
+imageAltText = "boo!"
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const DiscordRPC = require("discord-rpc");
+const { url } = require("inspector");
+const client = new DiscordRPC.Client({ transport: "ipc" }); // Create client instance
+require("dotenv").config(); // get .env file contents
+
+(async () => {
+  client.on("ready", async () => {
+    //vvvvv This runs when the client isready
+    await client
+      .setActivity({
+        // Setting the Rich Presence based on what is passed in here from our .env file.
+        buttons: [
+          {
+            label: process.env.label,
+            url: process.env.url,
+          },
+          {
+            label: process.env.labelTwo,
+            url: process.env.urlTwo,
+          },
+        ],
+        details: process.env.details,
+        largeImageKey: "rpc_icon",
+        largeImageText: process.env.imageAltTxet,
+      })
+      .catch((err) => console.log(err));
+
+    console.log("Discord Rich Presence has been enabled.");
+  });
+
+  await client
+    .login({ clientId: process.env.applicationID })
+    .catch(console.error); // Logging into our application.
+})();
